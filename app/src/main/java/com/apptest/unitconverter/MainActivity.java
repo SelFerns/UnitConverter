e32b6b6435ae83037571c8ad71bce324770fea07
SelFerns <selwynfernandes17@gmail.com>
2023-07-27 21:03:04 +0530

    Second Commit

--- app/src/main/java/com/apptest/unitconverter/MainActivity.java
+++ app/src/main/java/com/apptest/unitconverter/MainActivity.java
@@ -3,34 +3,29 @@
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
-import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
 import android.widget.Toast;
 
  public class MainActivity extends AppCompatActivity {
-    private Button button;
-    private TextView textView;
+     private TextView textView;
     private EditText editText;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        button = findViewById(R.id.button);
+        Button button = findViewById(R.id.button);
         textView = findViewById(R.id.textView);
         editText= findViewById(R.id.editText);
 
-        button.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Toast.makeText(MainActivity.this, "Calculated", Toast.LENGTH_SHORT).show();
-                String s = editText.getText().toString();
-                int kg = Integer.parseInt(s);
-                double pound = 2.205 * kg;
-                textView.setText(String.format("The corresponding value in Pound is: %s", pound));
-            }
+        button.setOnClickListener(view -> {
+            Toast.makeText(MainActivity.this, "Calculated", Toast.LENGTH_SHORT).show();
+            String s = editText.getText().toString();
+            int kg = Integer.parseInt(s);
+            double pound = 2.205 * kg;
+            textView.setText(String.format("The corresponding value in Pound is: %s", pound));
         });
     }
 }
